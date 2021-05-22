4bb6bfe8a9eefaacf836775c49182a83c756ab9a
Tejashri-hub <83937700+Tejashri-hub@users.noreply.github.com>
2021-05-22 13:55:13 +0530

    Add files via upload

---
+++ sign.js
@@ -0,0 +1,14 @@
+function validate()
+{
+    var username = document.getElementById('three');
+    var username = document.getElementById('two');
+
+    if(username.value=="" || password.value==" ")
+    {
+        alert("No blank values are allowed");
+        return false;
+    }
+    else{
+         true;
+    }
+}
